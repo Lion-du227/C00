2157415e75c03f9d20ac8890c27c1cbca4497939
Lion-du227 <ak.hainik@gmail.com>
2025-05-24 20:14:13 +0100

    Delete current_datetime.sh

--- current_datetime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Ce script affiche la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS".
-
-# %Y = année 	%m = mois %d = jour %H = heure %M = minute %S = seconde
-date "+%Y-%m-%d %H:%M:%S"
