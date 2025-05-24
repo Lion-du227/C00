ccf9ef76df7bca62468d14572b9a8d120a4f1807
Lion-du227 <ak.hainik@gmail.com>
2025-05-24 18:26:12 +0100

    Create current_datetime.sh

---
+++ current_datetime.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Ce script affiche la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS".
+
+# %Y = année 	%m = mois %d = jour %H = heure %M = minute %S = seconde
+date "+%Y-%m-%d %H:%M:%S"
