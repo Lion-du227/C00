d0336978fa668b870975a78651f8f0a68024623a
Lion-du227 <ak.hainik@gmail.com>
2025-05-25 17:13:48 +0100

    Create check_file_exist.sh

---
+++ check_file_exist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script pour vérifier si un fichier existe
+
+# Demander le nom du fichier à l'utilisateur
+echo "Entrez le nom du fichier : "
+read filename
+
+# Vérifier si le fichier existe
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
